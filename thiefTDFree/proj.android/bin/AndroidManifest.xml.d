a7a24c746bc881ae8d07f9e1dad7b18a27f319d1
Your Name <you@example.com>
2015-02-24 21:55:42 +0800

    e34tyytr
    rqwerqwer

---
+++ thiefTDFree/proj.android/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/workCode/BanditsComing/thiefTDFree/proj.android/bin/AndroidManifest.xml : \
+/workCode/BanditsComing/thiefTDFree/proj.android/AndroidManifest.xml \
---
+++ thiefTDFree/proj.android/bin/classes.dex.d
@@ -0,0 +1,9 @@
+/workCode/BanditsComing/thiefTDFree/proj.android/bin/classes.dex : \
+/workCode/BanditsComing/thiefTDFree/proj.android/bin/classes/com/BanditsComing/app2/BuildConfig.class \
+/workCode/BanditsComing/thiefTDFree/proj.android/bin/classes/com/BanditsComing/app2/R$attr.class \
+/workCode/BanditsComing/thiefTDFree/proj.android/bin/classes/com/BanditsComing/app2/R$drawable.class \
+/workCode/BanditsComing/thiefTDFree/proj.android/bin/classes/com/BanditsComing/app2/R$string.class \
+/workCode/BanditsComing/thiefTDFree/proj.android/bin/classes/com/BanditsComing/app2/R.class \
+/workCode/BanditsComing/thiefTDFree/proj.android/bin/classes/org/cocos2dx/cpp/Cocos2dxActivity.class \
+/workCode/BanditsComing/thiefTDFree/proj.android/bin/dexedLibs/classes-416e34fe7c9133e43b73bd2258dc7acc.jar \
+/workCode/BanditsComing/thiefTDFree/proj.android/bin/dexedLibs/annotations-4a43cda7fa5655a6a4d0b3e2cc1cd89b.jar \
